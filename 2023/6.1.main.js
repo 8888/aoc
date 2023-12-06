ba7b5e21b23ecf46f8457b0c546ad03fbd4fcfc3
8888 <leecostello1@gmail.com>
2023-12-06 00:25:58 -0500

    day 6 part 1

---
+++ 2023/6.1.main.js
@@ -0,0 +1,61 @@
+const { once } = require('node:events');
+const { createReadStream } = require('node:fs');
+const { createInterface } = require('node:readline');
+
+let total = 1;
+let times, distances;
+
+const parseLines = (line) => {
+  if (!times) {
+    times = line.match(/[0-9]+/g);
+    return;
+  } else if (!distances) {
+    distances = line.match(/[0-9]+/g);
+    return;
+  }
+}
+
+const doWork = () => {
+  times.forEach((time, i) => {
+    let wins = 0;
+    const low = Math.floor(parseInt(time) / 2);
+    let high = Math.ceil(parseInt(time) / 2);
+    high = low === high ? high + 1 : high;
+    const stack = [
+      { x: low, step: -1 },
+      { x: high, step: 1 },
+    ];
+    const distance = parseInt(distances[i]);
+    while (stack.length) {
+      const { x, step } = stack.pop();
+      const y = time - x;
+      if (x * y > distance) {
+        wins++;
+        stack.push({ x: x + step, step})
+      }
+    }
+    console.log("wins", wins)
+    total *= wins;
+  });
+};
+
+(async function processLineByLine() {
+  try {
+    const rl = createInterface({
+      input: createReadStream('6.1.input.txt'),
+      crlfDelay: Infinity,
+    });
+
+    rl.on('line', (line) => {
+      parseLines(line);
+    });
+
+    await once(rl, 'close');
+
+    doWork();
+    console.log(total)
+
+  } catch (err) {
+    console.error(err);
+  }
+})();
