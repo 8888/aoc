e6f1dc12de25f6d5d9d2c502b3d62be6f65fa4d0
8888 <leecostello1@gmail.com>
2024-12-19 23:18:39 -0500

    day 19.2

---
+++ 2024/19.2.py
@@ -0,0 +1,34 @@
+from functools import cache
+
+with open("19.txt") as file:
+  inputs = [line.rstrip() for line in file]
+
+towels = []
+designs = []
+total = 0
+
+parsedTowels = False
+for i in inputs:
+  if i == '':
+    parsedTowels = True
+  elif parsedTowels:
+    designs.append(i)
+  else:
+    towels = i.split(', ')
+
+@cache
+def search(design: str):
+  if design == "":
+    return 1
+
+  found = 0
+  for towel in towels:
+    if design.startswith(towel):
+      found += search(design[len(towel):])
+
+  return found
+
+for design in designs:
+  total += search(design)
+
+print(total)
