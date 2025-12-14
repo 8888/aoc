f41ec7d6cb34adfca4a440d83aa37429f0f96524
8888 <leecostello1@gmail.com>
2025-12-14 07:09:51 -0500

    2025.11.01

--- 2025/11.1.kts
+++ 2025/11.1.kts
@@ -1,12 +1,51 @@
 import java.io.File
 
-val map = HashMap<String, List<String>>()
-var total = 0
+class Graph {
+  private val edges = mutableMapOf<String, MutableList<String>>()
+
+  fun addNode(node: String) {
+    edges.putIfAbsent(node, mutableListOf<String>())
+  }
+
+  fun addEdge(from: String, to: String) {
+    addNode(from)
+    addNode(to)
+    edges[from]!!.add(to)
+  }
+
+  fun neighbors(node: String): List<String> =
+    edges[node].orEmpty()
+
+  fun countSimplePaths(start: String, target: String): Long {
+    if (start !in edges || target !in edges) return 0L
+
+    val inPath = HashSet<String>()
+
+    fun dfs(node: String): Long {
+      if (node == target) return 1L
+      if (!inPath.add(node)) return 0L // set.add returns a boolean! ez cycle detection!
+
+      var total = 0L
+      for (neighbor in neighbors(node)) {
+        total += dfs(neighbor)
+      }
+
+      inPath.remove(node)
+      return total
+    }
+
+    return dfs(start)
+  }
+}
+
+val graph = Graph()
 
 File("11.txt").forEachLine { line ->
   val devices = line.split("\\s|:\\s".toRegex()).toMutableList()
-  val key = devices.removeFirst()
-  map.put(key, devices)
+  val from = devices.removeFirst()
+  for (to in devices) {
+    graph.addEdge(from, to)
+  }
 }
 
-println("total $total")
+println(graph.countSimplePaths("you", "out"))
